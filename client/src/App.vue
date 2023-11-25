4ae52ce43846dcd2a4d7cbaf945ea2ff2a27498b
Your Name <you@example.com>
2023-11-25 17:13:29 -0300

    :feat: atualização do VueJs

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,4 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import axios from 'axios';
-
-axios.defaults.baseURL = 'http://localhost:3000';
+import { createApp } from 'vue'
+import App from './App.vue'
 
 createApp(App).mount('#app')
