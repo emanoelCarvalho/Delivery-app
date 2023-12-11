6a57bff44cd9fd5099a511d6398997d25c01c421
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2023-12-11 14:30:40 -0300

    :sparkles: feat(Insomnia.json): Adicionado file do insomnia, com as configurações de requests.

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const AdminController = require("../controllers/admin");
 
 router.post("/novoAdmin", AdminController.createAdmin);
-router.get("/listarAdmin", AdminController.getAdmins);
+router.get("/listarAdmins", AdminController.getAdmins);
 router.get("/getAdminById/:id", AdminController.getAdminById);
 router.patch("/updateAdmin/:id", AdminController.updateAdmin);
 router.delete("/deleteAdmin/:id", AdminController.deleteAdmin);
