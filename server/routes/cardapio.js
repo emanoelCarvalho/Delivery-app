685591c408e77b6926261530c54eb0875cf8397a
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2023-12-11 14:59:13 -0300

    :sparkles: feat(Insomnia.json): Adicionado file do insomnia, com as configurações de requests, para a rota de cardápio.

--- server/routes/cardapio.js
+++ server/routes/cardapio.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const cardapioController = require("../controllers/cardapio");
 
 router.post("/criarCardapio", cardapioController.createCardapio);
-router.get("/listarCardapio", cardapioController.getCardapios);
+router.get("/listarCardapios", cardapioController.getCardapios);
 router.get("/getCardapioById/:id", cardapioController.getCardapioById);
 router.patch("/updateCardapio/:id", cardapioController.updateCardapio);
 router.delete("/deleteCardapio/:id", cardapioController.deleteCardapio);
