caa8dea9838ae67c18f10dd3ef3a1f893b44d3d0
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2023-12-16 11:29:51 -0300

    :fire: remove: unused components and routes

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -23,22 +23,6 @@ const router = createRouter({
       name: "registerSideDish",
       component: () => import("@/components/SideDishRegister.vue"),
     },
-    {
-      path: "/register",
-      name: "register",
-      component: () => import("@/view/Cadastro.vue"),
-    },
-
-    {
-      path: "/PedidoCliente",
-      name: "PedidoCliente",
-      component: () => import("@/view/PedidoCliente.vue"),
-    },
-    {
-      path: "/PedidoCliente/finalizarPedido",
-      name: "finalizarPedido",
-      component: () => import("@/view/finalizarPedido.vue"),
-    },
   ],
 })
 
